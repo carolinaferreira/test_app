5bd1c289b636463a000a59c97e4655c05fd92ef8
carolinaferreira <carol.mf14@gmail.com>
2018-08-29 20:46:46 -0300

    Create show action and add gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,9 +27,11 @@ class UsersController < ApplicationController
         else
             render 'edit'
         end
-
     end
 
+    def show
+        @user = User.find(params[:id])
+    end
     private
     def user_params
         params.require(:user).permit(:username, :email,:password)
